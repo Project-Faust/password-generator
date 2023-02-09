d9f988ddf43de574e2a5801a164830be680bcfdf
Project Faust <pentecost.cody.lane@gmail.com>
2023-02-08 19:24:56 -0500

    added arrays for upper and lowercase letters, special characters, and digits  0-9

--- Develop/script.js
+++ Develop/script.js
@@ -13,19 +13,21 @@ function writePassword() {
 
 }
 
+var charactersUpper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var charactersLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var charactersSpecial = ["!", "@", "#", "$", "%", "^", "*"];
+var characterNumber = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 
 
 // click button to start sequence for generating pw
-if (confirm("Would you like to generate a password?", "8-128") == true) {
-  passwordLength = prompt("Between 8 and 128 characters, how many long would you like the password to be?");
+if (confirm("Would you like to generate a password?") == true) {
+  passwordLength = prompt("Between 8 and 128 characters, how many long would you like the password to be?", "8-128");
   passwordLengthNum = parseInt(passwordLength);
 } 
-// else {
-//   return
-// }
 
 
 // for loop to index array(s) for passwordLengthNum
